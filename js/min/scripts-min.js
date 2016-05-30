749a336e42a4e999c41d38bfbafc2dc1b807a7a3
Tiffaney Paris <tiffaneyparis@Tiffaneys-MacBook-Pro.local>
2016-05-30 15:00:40 -0700

    base commit

---
+++ js/min/scripts-min.js
@@ -0,0 +1,1 @@
+$(".cards").lightGallery({selector:"a"});
---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Basic, Single Image
+// $(".cards").lightGallery();
+
+// Group Images into a Gallery
+$(".cards").lightGallery({
+	selector: 'a'
+});
